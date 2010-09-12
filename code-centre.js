ebc585a645feb9da985384bb2d7a3f5417d955e1
Tay Ray Chuan <rctay89+samsung@gmail.com>
2010-09-12 19:50:31 +0800

    code-centre: centre all <pre> blocks, not just first one

--- code-centre.js
+++ code-centre.js
@@ -6,8 +6,10 @@ var old_centerSlides = centerSlides;
 centerSlides = function(p_slides) {
 	$(".content.code-centre", p_slides).each(function(s, slide) {
 		var slide$ = $(slide);
-		var code_block$ = $("pre", slide$).first();
-		code_block$.css("margin-left", (slide$.width() - $("code", code_block$).width()) / 2);
+		$("pre", slide$).each(function(i, code_block) {
+			var code_block$ = $(code_block);
+			code_block$.css("margin-left", (slide$.width() - $("code", code_block$).width()) / 2);
+		});
 	});
 	old_centerSlides(p_slides);
 }
